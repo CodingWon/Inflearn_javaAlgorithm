17064ef04a12193b4a7bdf46aaf52c9f75f0a750
CodingWon <kingcoding2298@gmail.com>
2022-03-20 17:26:46 +0900

    1. 문자 찾기

--- src/ch1/P1.java
+++ src/ch1/P1.java
@@ -1,10 +1,26 @@
 package ch1;
 
+import java.util.Scanner;
+
 public class P1 {
-
-
+    public static int solution(String str , char ch){
+        int cnt =0;
+        str = str.toUpperCase();
+        ch = Character.toUpperCase(ch);
+        for(int i = 0; i<str.length(); i++){
+            if(str.charAt(i) == ch){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char ch = sc.next().charAt(0);
 
+        int result = solution(str ,ch);
+        System.out.println(result);
     }
 }
